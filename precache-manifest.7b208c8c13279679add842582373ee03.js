a479e84e39197dd78a19458b1922bef385dc4946
mkohen <email>
2020-04-06 12:47:04 +0300

    Updates

---
+++ precache-manifest.7b208c8c13279679add842582373ee03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fed2865d29e0f726d09794d3f2480b",
+    "url": "/Expenses/index.html"
+  },
+  {
+    "revision": "efaaeadfcd2cdd2e1d53",
+    "url": "/Expenses/static/css/main.2541c6f9.chunk.css"
+  },
+  {
+    "revision": "5339c04e23ef5a9d428d",
+    "url": "/Expenses/static/js/2.43621562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expenses/static/js/2.43621562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efaaeadfcd2cdd2e1d53",
+    "url": "/Expenses/static/js/main.2d4d691c.chunk.js"
+  },
+  {
+    "revision": "927415bb4a4465f3472a",
+    "url": "/Expenses/static/js/runtime-main.1e23f7cb.js"
+  },
+  {
+    "revision": "3939a0894159661a36538308fa05f8ea",
+    "url": "/Expenses/static/media/bg.3939a089.jpg"
+  }
+]);
